7b92feec5551ee6979a84de85516e7f883b1c0a1
xujinghui <xujinghui@gmail.com>
2013-04-27 23:25:59 +0800

    TestCase

---
+++ base/src/test/java/com/github/xujh/demou/DemouTest.java
@@ -0,0 +1,14 @@
+package com.github.xujh.demou;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DemouTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
